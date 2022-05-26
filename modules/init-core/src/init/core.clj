637e6141291ea469614917a9e0115969cdc2a995
Ferdinand Beyer <ferdinand@wefarm.com>
2022-05-26 15:32:10 +0200

    Remove `init.protocols` namespace

--- modules/init-core/src/init/core.clj
+++ modules/init-core/src/init/core.clj
@@ -1,11 +1,1 @@
-(ns init.core
-  (:require [init.protocols :as protocols]))
-
-(extend-protocol protocols/Dependency
-  clojure.lang.Keyword
-  (-tag [kw] kw)
-  (-cardinality [_] :one)
-
-  clojure.lang.IPersistentMap
-  (-tag [m] (:tag m))
-  (-cardinality [m] (:cardinality m :one)))
+(ns init.core)
